c1fe676efd8028d54a8ecafb00d1a6c6fd18786e
Mona Lisa <jahanrothy58@gmail.com>
2024-04-17 17:16:03 +0600

    hero section complete

--- js/script.js
+++ js/script.js
@@ -3,76 +3,55 @@
 //    handle. ("activepage");
 // }
 
-console.log(
+function openMenupage() {
+  // e.preventDefault()
+  console.log("callded");
+  // console.log('even', e);
 
+  const menuPageElement = document.getElementsByClassName("menu_page");
 
+  const element = menuPageElement[0];
 
+  console.log(element);
 
-
-
-
-
-
-
-    'page'
-);
-
-function openMenupage(){
-    // e.preventDefault()
-    console.log('callded');
-    // console.log('even', e);
-
-    const menuPageElement = document.getElementsByClassName("menu_page");
-
-    const element = menuPageElement[0]
-    
-
-    console.log(element);
-
-    element.classList.add("active-menu-page");
-    // element.style.visibility ='visible';
-    
+  element.classList.add("active-menu-page");
+  // element.style.visibility ='visible';
 }
 
+function closeMenupage() {
+  // e.preventDefault()
+  console.log("callded");
+  // console.log('even', e);
 
-function closeMenupage(){
-    // e.preventDefault()
-    console.log('callded');
-    // console.log('even', e);
+  const menuPageElement = document.getElementsByClassName("menu_page");
 
-    const menuPageElement = document.getElementsByClassName("menu_page");
+  const element = menuPageElement[0];
 
-    const element = menuPageElement[0]
-    
+  console.log(element);
 
-    console.log(element);
-
-    element.classList.remove("active-menu-page");
-    // element.style.visibility ='visible';
-    
+  element.classList.remove("active-menu-page");
+  // element.style.visibility ='visible';
 }
 
 openMenupage();
 closeMenupage();
 
+// var swiper = new Swiper(".mySwiper", {
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//       renderBullet: function (index, className) {
+//         return '<span class="' + className + '">' + (index + 1) + "</span>";
+//       },
+//     },
+//   });
 
-
-// console.log(handle);
-// toggleMenupage();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 // function toggleMenupage(){
 //     var menuPages = document.getElementsByClassName('menupage');
@@ -85,7 +64,6 @@ closeMenupage();
 //     }
 //     console.log('kkdkk');
 // }
-
 
 // let div = document.getElementById('menupage');
 // let display = 0;
@@ -118,16 +96,13 @@ closeMenupage();
 //     }
 // }
 
-
 // const Swiper = new Swiper(".mySwiper", {
 //     pagination: {
 //       el: ".swiper-pagination",
 //       clickable: true,
 //       renderBullet: function (index, className) {
 //         return '<span class="' + className + '">"' + (index + 1) + "</span>";
-        
+
 //       },
-//     },  
+//     },
 //   });
-
-
